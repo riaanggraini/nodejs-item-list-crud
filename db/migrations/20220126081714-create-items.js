735f77a08111ed64ba089daaf5604684a43c291d
Ria Anggraini <r.anggraini9999@gmail.com>
2022-01-26 16:33:01 +0800

    create migration, seeders

---
+++ db/migrations/20220126081714-create-items.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      SKU: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      desc: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('items');
+  }
+};
---
+++ db/seeders/20220126082248-items.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('items', [
+    {
+      name: 'item name 1',
+      SKU: 'ITEM001',
+      desc: 'this is description of item 1',
+    },
+    {
+      name: 'item name 2',
+      SKU: 'ITEM002',
+      desc: 'this is description of item 2',
+    },
+    {
+      name: 'item name 3',
+      SKU: 'ITEM003',
+      desc: 'this is description of item 3',
+    },
+  ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+  }
+};
